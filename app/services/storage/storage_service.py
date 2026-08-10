ac4190daffa368ab4229d387fa2c2c39713a332d
jmssalas <hola@jmssalas.com>
2026-08-10 12:15:22 +0200

    Update method names

--- app/services/storage/storage_service.py
+++ app/services/storage/storage_service.py
@@ -7,11 +7,11 @@ class StorageService:
     def __init__(self, database: DatabaseInterface) -> None:
         self.db = database
 
-    def createConversation(self, data: Conversation) -> Conversation:
+    def create_conversation(self, data: Conversation) -> Conversation:
         return Conversation.model_validate(self.db.create(data))
 
-    def updateConversation(self, id: str, data: Conversation) -> Conversation:
+    def update_conversation(self, id: str, data: Conversation) -> Conversation:
         return Conversation.model_validate(self.db.update(id, data))
 
-    def listConversations(self) -> list[Conversation]:
+    def list_conversations(self) -> list[Conversation]:
         return [Conversation.model_validate(item) for item in self.db.list()]
