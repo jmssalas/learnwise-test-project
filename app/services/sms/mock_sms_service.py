348c2e6b8e081924745cff525e28912e07304572
jmssalas <hola@jmssalas.com>
2026-08-10 12:15:29 +0200

    Update method names

--- app/services/sms/mock_sms_service.py
+++ app/services/sms/mock_sms_service.py
@@ -3,5 +3,5 @@ from .sms_provider_interface import SMSProviderInterface
 
 
 class MockSMSProvider(SMSProviderInterface):
-    def send_sms(self, message: str) -> bool:
+    def send_sms(self, message: str, phone_number: str) -> bool:
         return True
--- app/services/sms/sms_provider_interface.py
+++ app/services/sms/sms_provider_interface.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class SMSProviderInterface(ABC):
     @abstractmethod
-    def send_sms(self, message: str) -> str:
+    def send_sms(self, message: str, phone_number: str) -> bool:
         pass
